2a3644fed588fbabaa36daf6af4717ed4b00b3cc
shreya maheshwari <62559289+shreya12-37@users.noreply.github.com>
2021-08-04 17:38:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+$('.footer-logo-extension').hide(); 
+$('.web-form-actions').hide();
+$('.btn-login-area').hide();
+$('.ml-2').hide();
+$('.section-head').css({"font-size":"13px","font-weight":"normal"});
+
+//value = frappe.web_form.get_value(do_you_provide_remote_simultaneous_conference_interpretation); 
+frappe.web_form.on('do_you_provide_remote_simultaneous_conference_interpretation', (field, value) => { 
+    console.log("inside the form") 
+    $('.ml-2').hide();
+    if (value=="No") { 
+        $('.ml-2').hide();
+        frappe.msgprint(__('Thank you for your interest in working with us. We are currently looking for interpreters with experience in remote simultaneous conference interpretation.')); 
+        field.set_value(0);
+        
+    }
+    if (value=="Yes"){
+        $('.ml-2').show();
+    }
+});
